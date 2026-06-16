5bfbf57baa7b3e9867a849ca67729c4a1b4af7f5
Luka Georgieski <89241332@student.upr.si>
2026-06-16 18:22:49 +0200

    Added Components, Context, Pages and Styles folder in frontend/src

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 30053,
+    host: '88.200.63.148'
+  }
 })
