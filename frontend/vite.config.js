1af364f725c38bd0b003cad2a30af287f911e725
Luka Georgieski <89241332@student.upr.si>
2026-06-16 21:52:10 +0200

    Updated vite.config so frontend forwards requests to backend port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 30053,
-    host: '88.200.63.148'
-  }
+    proxy: {
+      '/api': 'http://localhost:30052',
+    },
+  },
 })
